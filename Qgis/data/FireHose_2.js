535aaba89a958b70079f20ef1acdc1f14860a4bc
MythProg <etay.rab@gmail.com>
2020-06-09 14:30:33 +0300

    improve nav option

--- Qgis/data/FireHose_2.js
+++ Qgis/data/FireHose_2.js
@@ -3,6 +3,6 @@ var json_FireHose_2 = {
 "name": "FireHose_2",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-
+{ "type": "Feature", "properties": { "fid": "2827", "Type": "FireHose", "IntendFor": "organic solids such as paper and wood", "VisorNum": "0504014049", "VisorLoc": "שמורת נחל בניאס 21 א נתניה", "Picture": "https:\/\/www.mendelson.co.il\/Pictures\/6895.jpg?w=300", "MoreInfo": "במסתור ממול דלת הכניסה", "VisorName": "מייקל", "City": "נתניה", "Street": "שמורת נחל בניאס", "Number": 21.0 }, "geometry": { "type": "Point", "coordinates": [ 34.852303903811965, 32.291208282151402 ] } }
 ]
 }
